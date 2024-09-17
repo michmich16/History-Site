996f91997f447f4f0e1d8e4cc6251a6033a6dcb6
Michael P <102651473+michmich16@users.noreply.github.com>
2024-09-17 13:24:55 +0200

    r

--- History-React/src/components/Header/Header.jsx
+++ History-React/src/components/Header/Header.jsx
@@ -1,17 +1,21 @@
 import s from './Header.module.scss';
+import picture from '../../assets/images/header.png'
 
 export const Header = () => {
 
     return (
         <>
-            <hgroup className={s.headerStyle}>
-                <header className={s.headerContainer}>
-                    <div className={s.headerHero}>
-                        <h1>ON THIS DAY</h1>
-                        <h4>What happened on this day - historical events, deaths and births thoughout time</h4>
-                    </div>
-                </header>
-            </hgroup>
+            <header className={s.headerStyle}>
+                <img src={picture} alt="" />
+                <div className={s.headerHero}>
+                    <span className={`${s.circle} ${s.circleTopLeft}`}></span>
+                    <span className={`${s.circle} ${s.circleTopRight}`}></span>
+                    <h1>ON THIS DAY</h1>
+                    <h4>What happened on this day - historical events, deaths and births thoughout time</h4>
+                    <span className={`${s.circle} ${s.circleBottomLeft}`}></span>
+                    <span className={`${s.circle} ${s.circleBottomRight}`}></span>
+                </div>
+            </header>
         </>
     )
 }
--- History-React/src/components/Main/Main.jsx
+++ History-React/src/components/Main/Main.jsx
@@ -2,23 +2,37 @@ import s from './Main.module.scss';
 import { useState, useEffect } from 'react';
 
 export const Main = () => {
-
-    const [data, setData] = useState("");
+    const [data, setData] = useState(null);
 
     useEffect(() => {
-      const url = 'https://history.muffinlabs.com/date';
-  
-      fetch(url)
-        .then((response) => response.json())
-        .then((data) => console.log(data));
-    }, [data]);
+        const url = 'https://history.muffinlabs.com/date';
+
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => {
+                setData(data);
+            })
+            .catch((error) => console.error('Error fetching data:', error));
+    }, []);
+
     console.log(data);
 
     return (
         <>
-            <main>
-
+            <main className={s.contentStyle}>
+                {data ? (
+                    <div className={s.contentData}>
+                        {data.data?.Events?.map((event, index) => (
+                            <div key={index} className={s.event}>
+                                <p><strong>{event.year}</strong></p>
+                                <p>{event.text}</p>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <p>Loading...</p>
+                )}
             </main>
         </>
-    )
+    );
 }
